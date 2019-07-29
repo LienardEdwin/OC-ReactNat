966396149e61c02e0ab31171bb3ace3e136bb4bb
Lienard <edwin.lienard@outlook.fr>
2019-07-29 17:57:09 +0200

    MAJ gitignore + maj api

--- Components/Search.js
+++ Components/Search.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import {View,TextInput,Button,StyleSheet,FlatList,Text} from 'react-native'
-import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+import {getFilmsFromApiWithSearchedText} from "../API/TMDBApi"
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            films : [],
+            searchedText: ""
+        };
+    }
+
+    _searchTextInputChanged(text) {
+        this.setState({ searchedText: text })
+    }
+
+    _loadFilms() {
+        if (this.state.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+            getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
+                this.setState({ films: data.results })
+            })
+        }
+    }
+
     render() {
         return (
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {}}/>
+                <TextInput style={styles.textinput}
+                           placeholder='Titre du film'
+                           onChangeText={(text) => this._searchTextInputChanged(text)}
+                />
+                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />
